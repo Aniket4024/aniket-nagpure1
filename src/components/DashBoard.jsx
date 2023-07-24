a5d5ebeccdc5446bbe3319e6e979bf1ae1aaff82
Aniket Zade <aniketzade4024@gmail.com>
2023-07-24 12:44:14 +0530

    first commit

---
+++ src/components/DashBoard.jsx
@@ -0,0 +1,41 @@
+import style from "../css/Dashboard.module.css"
+import ColumnBars from "../graphs/ColumnBars"
+import PieChart from "../graphs/PieChart"
+import RingChart from "../graphs/RingChart"
+
+const Dashboard = ()=>{
+    
+
+    return <div id={style.Dashboard}>
+        <div>
+            <div id={style.ringChart}>
+                <div>
+                    <div>
+                        <RingChart percent={0.85}/>
+                    </div>
+                    <h4>$489,23.89 Sales</h4>
+                </div>
+                <div>
+                    <div>
+                        <RingChart percent={0.64}/>
+                    </div>
+                    <h4>Trafic</h4>
+                </div>
+                <div>
+                    <div>
+                        <RingChart percent={0.83}/>
+                    </div>
+                    <h4>893,89 Live Users</h4>
+                </div>
+            </div>
+            <div id={style.pieChart}>
+                <PieChart/>
+            </div>
+        </div>
+        <div>
+            <ColumnBars/>
+        </div>
+    </div>
+}
+
+export default Dashboard
